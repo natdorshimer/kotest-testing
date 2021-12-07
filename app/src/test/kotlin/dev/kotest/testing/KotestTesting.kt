878cbe3ccaf384e725fb6fb581ef30e24d65a67c
Natalie Dorshimer <dorshimer.natalie@gmail.com>
2021-12-07 13:40:04 -0500

    kotest testing

---
+++ app/src/test/kotlin/dev/kotest/testing/KotestTesting.kt
@@ -0,0 +1,13 @@
+package dev.kotest.testing
+
+import io.kotest.core.spec.style.FunSpec
+import kotlin.time.Duration.Companion.minutes
+
+
+class KotestTesting: FunSpec() {
+  init {
+    test("This will pass if project actually has a higher default than 10 mintues") {
+      Thread.sleep(11.minutes.inWholeMilliseconds)
+    }
+  }
+}
---
+++ app/src/test/kotlin/io/kotlintest/provided/AbstractProjectConfig.kt
@@ -0,0 +1,5 @@
+package io.kotlintest.provided
+
+import io.kotest.core.config.AbstractProjectConfig
+
+object ProjectConfig: AbstractProjectConfig()
