bb9659e6c5e576c52dbc2b8e0b3e7ef73dbae66a
Natalie Dorshimer <dorshimer.natalie@gmail.com>
2021-12-07 15:08:07 -0500

    trying to mimic the real project

--- app/src/test/kotlin/dev/kotest/testing/KotestTesting.kt
+++ app/src/test/kotlin/dev/kotest/testing/KotestTesting.kt
@@ -6,7 +6,7 @@ import kotlin.time.Duration.Companion.minutes
 
 class KotestTesting: FunSpec() {
   init {
-    test("This will pass if project actually has a higher default than 10 mintues") {
+    xtest("This will pass if project actually has a higher default than 10 mintues") {
       Thread.sleep(11.minutes.inWholeMilliseconds)
     }
   }
--- app/src/test/kotlin/io/kotlintest/provided/AbstractProjectConfig.kt
+++ app/src/test/kotlin/io/kotlintest/provided/AbstractProjectConfig.kt
@@ -1,5 +1,8 @@
 package io.kotlintest.provided
 
 import io.kotest.core.config.AbstractProjectConfig
+import kotlin.time.Duration.Companion.minutes
 
-object ProjectConfig: AbstractProjectConfig()
+object ProjectConfig: AbstractProjectConfig() {
+  override val projectTimeout = 12.minutes
+}
